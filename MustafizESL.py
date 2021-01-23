440beb8faee6862e6a0061294ba25a2acd7ff865
Mustafiz <70061121+MustafizESL@users.noreply.github.com>
2021-01-24 00:23:11 +0600

    Add templates

--- MustafizESL.py
+++ MustafizESL.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
 	{
 		'author': 'Mustafiz',
-		'title': 'How I mastered American Accent!',
+		'title': 'How I Mastered American Accent!',
 		'content': 'It was 2015. I used to struggle to understand native speakers, but I really wanted to speak like a native American. So, I started listening everyday. I listened to natural conversations of highly educated American language experts. I tried to copy them and I just loved it. After 2 months, I realized that my pronunciation was almost like a native American. I was surprised when I listened to my own voice which was recorded three months ago. I couldn\'t believe it!',
 		'date_posted': 'January 19, 2021'
 	},
@@ -15,7 +15,7 @@ posts = [
 		'date_posted': 'January 19, 2021'
 	}
 ]
-
+ 
 @app.route("/")
 @app.route("/home")
 def home():
@@ -24,7 +24,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == "__main__":
