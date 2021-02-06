d0f81aefbbeed4c2dd8b31ee34feb7c11f3c488d
Mustafiz <70061121+MustafizESL@users.noreply.github.com>
2021-02-06 22:55:09 +0600

    ADD Forms and User input

--- MustafizESL.py
+++ MustafizESL.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
+
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'e5b1a8127f65c1bd220a69b3379ef33d'
 
 posts = [
 	{
@@ -25,6 +31,22 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title = 'About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
