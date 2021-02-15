2141a25ba1f483c3740781f913be55b152a6bc4b
Mustafiz <70061121+MustafizESL@users.noreply.github.com>
2021-02-15 12:24:20 +0600

    Add User Authentication

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'e5b1a8127f65c1bd220a69b3379ef33d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from project import routes
